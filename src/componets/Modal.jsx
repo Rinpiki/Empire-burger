840c551b3ccf6e06fe6f8026877670aa55ec3e18
Marcos Pablo <rinpikibr@gmail.com>
2022-09-24 00:43:52 -0300

    erro de texto corrigido

--- src/componets/Modal.jsx
+++ src/componets/Modal.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Modal() {
   return (
     <div className="z-20 absolute top-[58px] backdrop-blur-sm left-0  bg-[#F0D8C2] -translate-x-72 animate-slide p-2 w-full sm:hidden ">
-      <ul className="  flex justify-center text-center flex-col items-center ">
+      <ul className="flex justify-center text-center flex-col items-center ">
         <li id="#">
           <a href="#" className="font-bold text-[#60340B]">
             Home
