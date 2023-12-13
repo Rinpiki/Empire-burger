8170e5234133d3dea500bbafa5fc1061d6a4e131
Rinpiki <rinpikibr@gmail.com>
2023-12-13 16:32:34 -0300

    fix: ptbr corrigido

--- src/componets/local/Local.jsx
+++ src/componets/local/Local.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 function Local() {
   return (
     <section className="w-full max-w-[60.625rem] flex items-center justify-center mb-[32px] flex-col text-center">
       <div>
         <h1 className="text-2xl font-LilitaOne uppercase text-[#1D0605E5] md:text-[32px]">
-          Onde ficar a nosso castelo
+          Onde fica o nosso castelo
         </h1>
         <p className="text-[#1D0605B0] text-base font-lato">
           Estaremos de portas abertas para a nossa realeza.
